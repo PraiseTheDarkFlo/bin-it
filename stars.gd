a30bd50d701da355ebe9b67dc41d8e99ed7a1d77
mmdark <fhock2002@gmail.com>
2025-06-10 12:51:34 +0200

    updated update stars

--- stars.gd
+++ stars.gd
@@ -4,3 +4,5 @@ func set_stars(count: int) -> void:
 	if count > 0 and count <= get_child_count():
 		for i in range(count):
 			get_child(i).play("Full")
+		for i in range(count,get_child_count()):
+			get_child(i).play("Empty")
